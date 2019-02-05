436f5059d1cb67b9d3c9ba9544fa96667540c24a
Lubin1611 <43751457+Lubin1611@users.noreply.github.com>
2019-02-05 15:45:22 +0100

    Form inscription et connection

---
+++ inscription.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrateur
+ * Date: 05/02/2019
+ * Time: 14:53
+ */
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Bienvenue sur Game_zeldop</title>
+</head>
+<style>
+
+    body {
+        display: flex;
+        width: 99%;
+        height: 95vh;
+        justify-content: center;
+        align-items: center;
+
+    }
+    form {
+
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+
+
+    }
+
+    #inscription {
+        text-align: center;
+    }
+
+</style>
+<body>
+
+<div id = "inscription">
+<h1>Bienvenue sur Game_zeldop ! </h1>
+
+<h2>Inscription</h2>
+<form action="" method="post">
+    <label>Votre Nom : </label><input type="text" name = "nom"><br>
+    <label>Votre Prenom : </label><input type="text" name = "prenom"><br>
+    <label>Votre Pseudo : </label><input type = "text" name = "pseudo"><br>
+    <label>Votre e-mail : </label><input type="text" name = "mail"><br>
+    <label>Votre mot de passe : </label><input type="password" name = "mdp"><br>
+    <input type ="submit" value="Inscrivez-vous">
+    <br>
+    <a href="index.php">Déjà inscrit ? Cliquez pour vous connecter</a>
+</form>
+</div>
+
+
+</body>
+</html>
