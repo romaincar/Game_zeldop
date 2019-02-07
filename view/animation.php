9705adacf7dad62f1d0bc5c3722b31ffbceca7b5
darkoni59 <darkoni59610@gmail.com>
2019-02-07 15:26:15 +0100

    game zeldop

--- view/animation.php
+++ view/animation.php
@@ -19,8 +19,23 @@
 <body>
 <div id="viewport">
     <div id="field" style="height: 700px; width: 700px; border: solid black" >
+        <table style="border: solid white;height: 44em;width: 44em">
+            <TR> <TD><br><br></TD><TD></TD>
+                <TD></TD> <TD></TD> </TR>
+            <TR valign=top> <TD><BR><br></TD> <TD></TD>
+                <TD align=center></TD> <TD></TD> </TR>
+            <TR valign=bottom> <TD><BR><br></TD> <TD></TD>
+                <TD></TD> <TD valign=top></TD> </TR>
+            <TR align=right> <TD><BR><br></TD> <TD align=top></TD>
+                <TD></TD> <TD valign=bottom></TD> </TR>
+        </table>
+    <div id="kdeplacement" style="top:200px; left:200px;">
+        <div id="option">
+        <input type="submit" value="atk" id="attack">
+        <input type="submit" value="def" id="defence">
+        </div>
 
-    <div id="kdeplacement" style="top:200px; left:200px;"></div>
+    </div>
 
 </div>
 </div>
@@ -36,6 +51,14 @@ background-image: url("../Images/background.jpg");
         background-image: url("../Images/trees-and-bushes.png");
         background-size: 200px;
     }
+   table{
+       display: table;
+       border: solid white;
+       position: absolute;
+
+       height: 50px;
+       width: 50px;
+   }
 
     #viewport
     {
@@ -45,25 +68,24 @@ background-image: url("../Images/background.jpg");
         margin: auto;
 
     }
+    td{
+        border-style: double ;
 
-    #kdeplacement
-    {
-        background-image:url("../Images/armored_skeleton_slash.gif");
+    }
+
+    #kdeplacement {
+        background-image: url("../Images/armored_skeleton_slash.gif");
         width: 60px;
         height: 65px;
         position: absolute;
         top: 100px;
         left: 100px;
+        right: 100px;
+        bottom: 100px;
     }
-
-    #buttons
-    {
-        text-align: center;
-        margin-top: 25px;
-    }
-
-    }
-
+#option{
+    display: inline-flex;
+}
 
 </style>
 <script>
@@ -79,7 +101,7 @@ background-image: url("../Images/background.jpg");
 
 
 
-            case 38: //haut
+            case 38: //deplacement haut;
 
                 document.getElementById('kdeplacement').style.top = (y--*10) +"px" ;
                 if(y <1){
@@ -88,29 +110,53 @@ background-image: url("../Images/background.jpg");
                 break;
 
 
-            case 40: // bas
+            case 40: // deplacement bas;
                 document.getElementById('kdeplacement').style.top = (y++*10) +"px";
-                if(y>439){
-                    y=540;
+                if(y>60){
+                    y=0;
                 }
                 break;
 
-            case 37: //gauche
+            case 37: //deplacement gauche;
                 document.getElementById('kdeplacement').style.left = (x--*10) +"px";
                 if(x <1){
                     x=0;
                 }
                 break;
-            case 39 :   // droite;
+            case 39 :   // deplacement droite ;
                 document.getElementById('kdeplacement').style.left = (x++*10) +"px";
-                if(x >1139){
-                    x=540;
+                if(x >60){
+                    x=0;
                 }
                 break;
 
         }
     }
     document.body.addEventListener("keydown", deplacement);
+
+    function attack(a) {
+        document.getElementById('attack');
+        if (a=='onclick'){
+        alert('bone attack')
+        }
+
+    }
+    document.body.addEventListener("onclick",attack);
+
+    function experience (){
+        document.getElementById('kdeplacement')
+
+    }
+
+    function popmob(){
+
+    }
+    function vie (v){
+     if (v==0){
+         alert('game over')}
+
+
+    }
 
 
 </script>
